39f9f9fb1f0924330f61801740813d0e3f316da6
Volodymyr Bohdan <vovabohdan4@gmail.com>
2022-10-20 22:12:41 +0300

    Implement lab4

---
+++ lab4/README.md
@@ -0,0 +1,3 @@
+To start the project, run folowing command in the terminal from dir lab4:
+
+# node app.js
---
+++ lab4/app.js
@@ -0,0 +1,91 @@
+const mongoose = require("mongoose");
+const express = require("express");
+const Schema = mongoose.Schema;
+const app = express();
+const jsonParser = express.json();
+
+const productScheme = new Schema(
+    {
+        name: String,
+        price: Number
+    },
+    {
+        versionKey: false
+    }
+);
+const Product = mongoose.model("Product", productScheme);
+
+app.use(express.static(__dirname + "/public"));
+
+mongoose.connect("mongodb://localhost:27017/usersdb", { useUnifiedTopology: true, useNewUrlParser: true }, function (err) {
+    if (err) return console.log(err);
+    app.listen(3000, function () {
+        console.log("The server is waiting for a connection...");
+    });
+});
+
+
+app.get("/api/products", function (req, res) {
+
+    Product.find({}, function (err, products) {
+        if (err) return console.log(err);
+        res.send(products)
+    });
+});
+
+app.get("/api/products/:id", function (req, res) {
+
+    const id = req.params.id;
+    Product.findOne({ _id: id }, function (err, product) {
+        if (err) return console.log(err);
+        res.send(product);
+    });
+
+});
+
+app.post("/api/products", jsonParser, function (req, res) {
+
+    if (!req.body) return res.sendStatus(400);
+
+    const productName = req.body.name;
+    const productPrice = req.body.price;
+    const product = new Product({ name: productName, price: productPrice });
+
+    product.save(function (err) {
+        if (err) return console.log(err);
+        res.send(product);
+    });
+
+});
+
+app.delete("/api/products/:id", function (req, res) {
+
+    const id = req.params.id;
+    Product.findByIdAndDelete(id, function (err, product) {
+        if (err) return console.log(err);
+        res.send(product);
+    });
+
+});
+
+app.put("/api/products", jsonParser, function (req, res) {
+
+    if (!req.body) return res.sendStatus(400);
+    const id = req.body.id;
+    const productName = req.body.name;
+    const productPrice = req.body.price;
+    const newProduct = { name: productName, price: productPrice };
+
+    Product.findOneAndUpdate({ _id: id }, newProduct,
+        { new: true },
+        function (err, product) {
+            if (err) return console.log(err);
+            res.send(product);
+        });
+});
+
+// прослушиваем прерывание работы программы (ctrl-c)
+//process.on("SIGINT", () => {
+//    dbClient.close();
+//    process.exit();
+//});
